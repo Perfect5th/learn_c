d1f5d654ec8b6efbcf5b9ae3a7cec7b87830e1e1
perfect5th <mitch.b45@gmail.com>
2016-12-28 11:55:30 -0800

    Add .gitignore, ignoring compiled files

---
+++ chapter_1/exercise_1-8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* counts blanks, tabs, and newlines */
+main()
+{
+    int c, count;
+    
+    count = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n')
+            ++count;
+        if (c == ' ')
+            ++count;
+        if (c == '\t')
+            ++count;
+    }
+    printf("%d\n", count);
+}
